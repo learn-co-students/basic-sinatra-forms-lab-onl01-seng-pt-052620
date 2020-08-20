ea82b50ddb696d09f183869dfdc071eda1cc43e6
Robert Salaiz <work.salaizrm@gmail.com>
2020-08-20 22:53:08 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,21 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @team_name = "name"
+    @coach = "coach"
+    @point_guard = "pg"
+    @shooting_guard = "sg"
+    @small_forward = "sf"
+    @power_forward = "pf"
+    @center = "c"
+
+    erb :team
+  end
+
 
 end
